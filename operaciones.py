403e1ac09a8c36d2acba88dd142376d02dc8b736
Yohel Amat <yohel.amat@uip.edu.pa>
2025-11-21 14:03:30 +0000

    cree el modulo de operaciones donde se incluyen las operaciones de sumar, restar, multiplicar y dividir. Y en el caso de dividir cree el error en caso de que el usuario divida entre 0.
    git sTATUS

---
+++ operaciones.py
@@ -0,0 +1,14 @@
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "Error: No se puede dividir por cero."
+    return a / b
+
